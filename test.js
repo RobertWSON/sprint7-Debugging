94d680829082d6a0559d3ead2f7b126530198497
robjwilkinson@hotmail.com <robert@Roberts-MacBook-Air.local>
2018-07-23 01:02:54 +1200

    7.4 Debugging - change testing code

--- test.js
+++ test.js
@@ -6,6 +6,6 @@ var n = 0
 
 for (var i = 0; i < 10; i++)    {
     n = addOne(n)
-    console.log('The value of 'n' is', n)
-    document.write('The value of 'n' is ', n, '<br>')
-}
+    console.log('The value of `n` is', n)
+    document.write('The value of `n` is ', n, '<br>')
+};
